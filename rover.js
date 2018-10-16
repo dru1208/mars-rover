06899546b78bf1fbe84fddc5a46cb814a7286684
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-16 14:27:50 -0400

    created rotate left method, passes tests for cardinal direction

--- rover.js
+++ rover.js
@@ -4,6 +4,26 @@ class Rover {
     this.y = y_value;
     this.cardinal_direction = cardinal_direction
   }
+
+  rotateLeft () {
+    switch (this.cardinal_direction) {
+      case "N":
+        this.cardinal_direction = "W";
+        break;
+
+      case "W":
+        this.cardinal_direction = "S";
+        break;
+
+      case "S":
+        this.cardinal_direction = "E";
+        break;
+
+      case "E":
+        this.cardinal_direction = "N";
+        break;
+    }
+  }
 }
 
 module.exports = Rover;
