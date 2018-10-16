673d62dd0aafc1f83963a5c5bda945a1b2d5f2e2
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-16 14:08:20 -0400

    rover test for rover class constructor

--- tests/grid-spec.js
+++ tests/grid-spec.js
@@ -13,14 +13,15 @@ describe("Grid", () => {
 })
 
 
-describe("New Grid x and y value", () => {
-  it ("should create a new instance of Grid with an x value of the first argument and a y value of the second argument", () => {
-    let Grid1 = new Grid(3, 5);
+describe("New Grid Instance", () => {
+  let Grid1 = new Grid(3, 5);
+  let Grid2 = new Grid(8, 2);
+  it ("should create a new instance of Grid with the first argument as the x property", () => {
     expect(Grid1.x).to.equal(3);
-    expect(Grid1.y).to.equal(5);
-
-    let Grid2 = new Grid(8, 2);
     expect(Grid2.x).to.equal(8);
+  })
+  it ("should create a new instance of Grid with the second argument as the y property", () => {
+    expect(Grid1.y).to.equal(5);
     expect(Grid2.y).to.equal(2);
   })
 })
--- tests/rover-spec.js
+++ tests/rover-spec.js
@@ -13,3 +13,19 @@ describe("Rover", () => {
 })
 
 
+describe("New Rover Instance", () => {
+  let Rover1 = new Rover(2, 3, "N")
+  let Rover2 = new Rover(5, 6, "S")
+  it ("should create a new instance of Rover with the first argument as the x coordinate property", () => {
+    expect(Rover1.x).to.equal(2);
+    expect(Rover2.x).to.equal(5);
+  })
+  it ("should create a new instance of Rover with the second argument as the y coordinate property", () => {
+    expect(Rover1.y).to.equal(3);
+    expect(Rover2.y).to.equal(6);
+  })
+  it ("should create a new instance of Rover with the third argument as the cardinal direction property", () => {
+    expect(Rover1.cardinal_direction).to.equal("N");
+    expect(Rover2.cardinal_direction).to.equal("S");
+  })
+})
