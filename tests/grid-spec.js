b92ab42072db573723a7b64cd5793d430742c4ed
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-16 13:18:54 -0400

    adding mocha recusive terminal command, testing it out

--- tests/grid-spec.js
+++ tests/grid-spec.js
@@ -3,8 +3,22 @@
 const { expect } = require('chai');
 const Grid = require('../grid.js');
 
+
 describe("Grid", () => {
   it ("should exist", () => {
     expect(Grid).to.not.be.undefined;
+  })
+})
+
+
+describe("New Grid x and y value", () => {
+  it ("should create a new instance of Grid with an x value of the first argument and a y value of the second argument", () => {
+    let Grid1 = new Grid(3, 5);
+    expect(Grid1.x).to.equal(3);
+    expect(Grid1.y).to.equal(5);
+
+    let Grid2 = new Grid(8, 2);
+    expect(Grid2.x).to.equal(8);
+    expect(Grid2.y).to.equal(2);
   })
 })
--- tests/rover-spec.js
+++ tests/rover-spec.js
@@ -1,1 +1,10 @@
 // Tests for the rover class definition
+
+const { expect } = require('chai')
+const a = 12
+
+describe("mocha recurisve test", () => {
+  it ("should be 12", () => {
+    expect(a).to.equal(12);
+  })
+})
