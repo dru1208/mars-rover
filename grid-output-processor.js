a71a2b0e6c49cf40f65f46e69047308ae458421b
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-18 19:39:34 +0000

    output processor class created with basic test of existing

---
+++ grid-output-processor.js
@@ -0,0 +1,5 @@
+class GridOutputProcessor {
+
+}
+
+module.exports = GridOutputProcessor;
---
+++ tests/05-grid-output-processor-spec.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const GridOutputProcessor = require("../grid-output-processor.js");
+
+describe("Grid Output Processor", () => {
+  it ("should exist", () => {
+    expect(GridOutputProcessor).to.not.be.undefined;
+  })
+})
