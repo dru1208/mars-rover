b8c2b6d45b77ce114164c7a18cdd2e72d143a83c
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-10-18 00:01:07 +0000

    added a grid input test for generating grid info object

--- tests/03-grid-input-processor-spec.js
+++ tests/03-grid-input-processor-spec.js
@@ -6,3 +6,14 @@ describe("Grid Input Processor", () => {
     expect(GridInputProcessor).to.not.be.undefined;
   })
 })
+
+
+describe("#generateGridInfoObject", () => {
+  it ("should return an object with values that match keys for x and y", () => {
+    let processor = new GridInputProcessor;
+    let stringInput = "5 5";
+    let answerObject = {x: 5, y: 5};
+    gridInfo = processor.generateGridInfoObject(string);
+    expect(gridInfo).to.eql(answer);
+  })
+})
